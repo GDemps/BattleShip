13ca4d5fd4b643904cca5beaeb9efe34bd494cea
Grant Dempsey <grant.dempsey.1@Grants-MacBook-Pro.local>
2020-03-28 22:16:33 -0600

    Finished Overlapping Adjustment in valid_coordinate? Method

--- test/board_test.rb
+++ test/board_test.rb
@@ -71,13 +71,23 @@ class BoardTest < Minitest::Test
     assert_equal cruiser, board.cells["A1"].ship
     assert_equal cruiser, board.cells["A2"].ship
     assert_equal cruiser, board.cells["A3"].ship
-    binding.pry
   end
 
   def test_cells_all_have_ships
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     board.place(cruiser, ["A1", "A2", "A3"])
+  end
+
+  def test_can_ships_overlap
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
     binding.pry
+    assert_equal false, board.valid_placement?(submarine, ["A1", "B2"])
   end
 end
