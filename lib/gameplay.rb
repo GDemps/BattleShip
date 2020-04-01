0187e53b84b3118d5f25a92c8e3c0f999605daeb
Grant Dempsey <grant.dempsey.1@Grants-MacBook-Pro.local>
2020-03-31 19:47:55 -0600

    Finished place_computer_ships method in gameplay

--- lib/gameplay.rb
+++ lib/gameplay.rb
@@ -20,6 +20,7 @@ class Gameplay
           cruiser = Ship.new("Cruiser", 3)
           @cruiser = cruiser
           submarine = Ship.new("Submarine", 2)
+          @submarine = submarine
           place_computers_ships
           #computer uses the place_ship method to
           place_human_player_ships
@@ -32,21 +33,6 @@ class Gameplay
 
   def place_computers_ships
     #choices = @game_board.cells.keys
-    cruiser_array_of_options = [["A1", "A2", "A3"], ["A2", "A3", "A4"],
-                        ["B1", "B2", "B3"], ["B2", "B3", "B4"],
-                        ["C1", "C2", "C3"], ["C2", "C3", "C4"],
-                        ["D1", "D2", "D3"], ["D2", "D3", "D4"],
-                        ["A1", "B1", "C1"], ["B1", "C1", "D1"],
-                        ["A2", "B2", "C2"], ["B2", "C2", "D2"],
-                        ["A3", "B3", "C3"], ["B3", "C3", "D3"],
-                        ["A4", "B4", "C4"], ["B4", "C4", "D4"]]
-    computer_place_cruiser_coordinate = []
-    computer_place_cruiser_coordinate << cruiser_array_of_options.sample
-
-    if @game_board.valid_placement?(@cruiser, computer_place_cruiser_coordinate.flatten) == true
-      @game_board.place(@cruiser, computer_place_cruiser_coordinate.flatten)
-    end
-
     sub_array_of_options = [["A1", "A2"],
                             ["A2", "A3"],
                             ["A3", "A4"],
@@ -74,20 +60,25 @@ class Gameplay
 
     computer_place_sub_coordinate = []
     computer_place_sub_coordinate << sub_array_of_options.sample
-
-    if @game_board.valid_placement?(@submarine, computer_place_sub_coordinate.flatten) == true
+      @game_board.valid_placement?(@submarine, computer_place_sub_coordinate.flatten) == true
       @game_board.place(@submarine, computer_place_sub_coordinate.flatten)
+
+    cruiser_array_of_options = [["A1", "A2", "A3"], ["A2", "A3", "A4"],
+                        ["B1", "B2", "B3"], ["B2", "B3", "B4"],
+                        ["C1", "C2", "C3"], ["C2", "C3", "C4"],
+                        ["D1", "D2", "D3"], ["D2", "D3", "D4"],
+                        ["A1", "B1", "C1"], ["B1", "C1", "D1"],
+                        ["A2", "B2", "C2"], ["B2", "C2", "D2"],
+                        ["A3", "B3", "C3"], ["B3", "C3", "D3"],
+                        ["A4", "B4", "C4"], ["B4", "C4", "D4"]]
+    computer_place_cruiser_coordinate = []
+    computer_place_cruiser_coordinate << cruiser_array_of_options.sample
+
+    if @game_board.valid_placement?(@cruiser, computer_place_cruiser_coordinate.flatten) == true
+      @game_board.place(@cruiser, computer_place_cruiser_coordinate.flatten)
     end
     binding.pry
   end
-
-
-
-
-
-
-      #return @game_board.place(@cruiser, [choices.sample, choices.sample, choices.sample])
-
 
   def place_human_player_ships
     "I have laid out my ships on the grid.
